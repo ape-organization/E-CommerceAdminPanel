248b9740c57208b09fb8304fcb270b9c35a617a7
Pola Nasser <121816986+POlaNasserSamouel@users.noreply.github.com>
2026-09-08 10:26:49 +0300

    Update API and image base URLs for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://ecommercepinkyauraapi.ape-org.com/api',
-    imageBaseUrl: 'https://ecommercepinkyauraapi.ape-org.com'
+  apiBaseUrl: 'https://ecommerceapi.ape-org.com/api',
+    imageBaseUrl: 'https://ecommerceapi.ape-org.com'
 
 };
